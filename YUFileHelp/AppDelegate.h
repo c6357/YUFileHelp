472163445b3810914a0f779fda4e616bae4fa6ef
6357 <c6357@outlook.com>
2015-08-02 17:20:33 +0800

    Initial Commit

---
+++ YUFileHelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YUFileHelp
+//
+//  Created by yuzhx on 15/8/2.
+//  Copyright (c) 2015年 BruceYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YUFileHelp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YUFileHelp
+//
+//  Created by yuzhx on 15/8/2.
+//  Copyright (c) 2015年 BruceYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
